ec11890a78b3f859712ad2c4cbed6dd3935e2353
Taylorme5 <123215558+Taylorme5@users.noreply.github.com>
2023-12-10 14:34:47 -0500

    Refining main and game

--- SudokuMain.hs
+++ SudokuMain.hs
@@ -38,10 +38,12 @@ playGameOption = do
             puzzleGrid <- Generator.removeNumbers 30 completedGrid
             let candidates = Game.emptyCandidates
             putStrLn "How to play:"            
-            putStrLn "-Enter row, column, and number (e.g., 2 3 5) or 'q' to quit:"
-            putStrLn "-To add candidates type 'addCandidate:' followed by row, column, and number."
-            putStrLn "-To remove candidates type 'removeCandidate:' followed by row, column, and number."
-            putStrLn "-To a list of candidates at any cell type 'getCandidates:' followed by the row and column.\n"
+            putStrLn "-Enter row, column, and number (e.g., 2 3 5)"
+            putStrLn "-To clear cells type 'clear:' followed by row and column."
+            putStrLn "-To add candidates type 'addCand:' followed by row, column, and number."
+            putStrLn "-To remove candidates type 'removeCand:' followed by row, column, and number."
+            putStrLn "-To a list of candidates at any cell type 'getCandids:' followed by the row and column."
+            putStrLn "-Enter 'q' to quit.\n"
             Game.playGame puzzleGrid candidates
         Nothing -> putStrLn "No solution found"
     mainMenu
@@ -69,6 +71,6 @@ mainMenu = do
         "1" -> generatePuzzleOption
         "2" -> solvePuzzleOption
         "3" -> playGameOption
-        "4" -> putStrLn "Exiting the program."
-        _   -> putStrLn "Invalid option, please try again." >> mainMenu
+        "4" -> putStrLn "\nSee you later loser!!!!!"
+        _   -> putStrLn "Invalid option. Try again." >> mainMenu
 
